f79780f0e2ad5ccb227b5728c245e18b2422af45
Chisa <chisalaluckyk5@gmail.com>
2026-02-22 15:04:38 +0200

    fjjfjvjfjvj

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,18 +52,7 @@ return [
         // Just reference the disk name you selected in the Infrastructure Canvas
         // Example: if you named it 'r2' or 'properties-storage'
 
-        'uploads' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'auto'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
-        ],
+
 
     ],
 
